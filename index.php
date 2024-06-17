cabecb148640ff3d22fd21437d4fbccebadf42d2
Benjamin383 <brieucau@gmail.com>
2024-06-17 12:20:44 +0200

    Création de layout.php

--- index.php
+++ index.php
@@ -5,3 +5,8 @@ require_once 'config.php';
 
 
 ?>
+
+<?php
+$title = "Titre de la page";
+$content = ob_get_clean();
+include 'layout.php';
---
+++ layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title ?></title>
+</head>
+<body class="">
+    <div class="">
+        <header class="">
+            <h1 class="">Gestion des Utilisateurs</h1>
+            <nav>
+                <a href="index.php" class="">Accueil</a>
+                <a href=".php" class="">Créer un quiz</a>
+            </nav>
+        </header>
+        <main>
+            <?php echo $content ?>
+        </main>
+    </div>
+</body>
+</html>
+
