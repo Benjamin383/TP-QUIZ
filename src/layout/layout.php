efb6332b9c7e1c03383b5d39d1d10d0a542347fd
Anthony LUC <anthony.luc69@gmail.com>
2024-06-20 09:38:19 +0200

    add jquery

--- src/layout/layout.php
+++ src/layout/layout.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script differ src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title><?php echo $title ?></title>
 </head>
 <body class="">
