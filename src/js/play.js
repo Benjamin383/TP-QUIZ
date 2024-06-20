1413ad0f4ce635ae96d3fbcbc27b5a29c22dcaf0
Anthony LUC <anthony.luc69@gmail.com>
2024-06-20 15:59:07 +0200

    quiz en cours

--- src/js/play.js
+++ src/js/play.js
@@ -3,22 +3,69 @@ $(document).ready(function () {
     $("h1").text("question 1");
     let response_form = $("#response_form");
       response_form.empty();
+    const id_quiz_var = 2;
+    $("h1").after("<div id='score'>0</div>");
+    let score = $("#score");
+    let counterQuestion = 0;
 
-    $.ajax({url:"play_get_question.php", 
-      method: "POST", 
-      data:{id_quiz: "2"}, 
-      datatype: "json"
-
-    }).done(function(data){
-
-      response_form.html(data);
+    $.ajax({
+      url:"play_get_question.php", 
+      type: "POST", 
+      data:{id_quiz: id_quiz_var}, 
+      success: function (data) {
       
-    }).fail(function() {
-      // J'affiche un message d'erreur
-      response_form.html("Aucun résultat trouvé.");
-    })    
-  });
+      console.log(data);
+      $("h1").html(data[0].question);
+
+      let options = data[0].options.split(",");
+      $.each(options, function( index, value ) {
+        let str = value.replace("[","").replace("]","");
+        $("#response_form").append( "<button type='button' class='option' name='option' value='"+ ++index +"'>" + str + "</button>");
+      });
+
+      //Lorsqu'on clique sur une réponse
+      $(".option").click(function(e){
+        e.preventDefault();
+
+        //Si c'est correct, on augmente le score
+        if($(this).prop("value") === data[0].correctAnswer){
+          let number = parseInt(score.text());
+          score.html(++number);
+        }
+      });
+
+
+
+    },
+      error: function (error) {
+        console.error('Error uploading questions :', error);
+      }
+    });
+  
+  
+  // ).done(function(data){
+
+  //     response_form.html(data);
+      
+  //   }).fail(function() {
+  //     // J'affiche un message d'erreur
+  //     response_form.html("Aucun résultat trouvé.");
+  //   });
     
+
+  //   $.ajax({
+  //     url: apiUrl + 'photos',
+  //     type: 'POST',
+  //     data: formData,
+  //     processData: false,
+  //     contentType: false,
+  //     success: function () {
+  //         fetchPhotos();
+  //     },
+  //     error: function (error) {
+  //         console.error('Error uploading photos:', error);
+  //     }
+  // });
 
     // Lancement du timer
     $.getScript("/js/timer_play.js");
--- src/play/play_get_question.php
+++ src/play/play_get_question.php
@@ -1,9 +1,7 @@
 <?php
 require_once '../config.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
     $id_quiz = $_POST['id_quiz'];
     $sql_question = "SELECT q.question, q.options, q.correctAnswer 
     FROM question q
@@ -11,11 +9,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     WHERE qq.id_quiz = $id_quiz
     ORDER BY qq.id_question";
 
-    $result_question = $conn->query($sql_question);
+    $result = $conn->query($sql_question);
+    $questions = $result->fetch_all(MYSQLI_ASSOC);
     header("Content-Type: application/json");
-    echo json_encode($result_question);
-    exit();
+    echo json_encode($questions);
 }
-
-
-?>
