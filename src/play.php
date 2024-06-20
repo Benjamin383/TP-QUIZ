89e6aec1750db7827dd8304882626221d4267075
Benjamin383 <brieucau@gmail.com>
2024-06-20 09:52:52 +0200

    Index margin

--- src/play.php
+++ src/play.php
@@ -9,13 +9,14 @@ $result_quiz = $conn->query($sql_quiz);
 $quizs = $result_quiz->fetch_assoc();
 
 ?>
-
-<h1 class="text-center"><?= $quizs['titre'] ?></h1>
-<form action="" method="POST">
-    <div class="d-grid gap-2 col-6 mx-auto">
-        <button type="submit" class="btn btn-success">Jouer</button>
-    </div>
+<div class="container justify-content-center align-items-center mx-auto mt-5">
+    <h1 class="text-center"><?= $quizs['titre'] ?></h1>
+    <form action="" method="POST">
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <button type="submit" class="btn btn-success">Jouer</button>
+        </div>
 </form>
+</div>
 <?php
 $title = "Quiz";
 $content = ob_get_clean();
